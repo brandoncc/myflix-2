e2a0edc97162358c114ca329d41052d5530d8f49
Alex Henegar <alex.henegar19@gmail.com>
2018-01-23 09:55:55 -0500

    build home, video show, categories, and show category page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  get '/home', to: 'videos#index', as: 'home'
+  resources :videos, only: [:index, :show]
+  resources :categories, only: [:show]
 end
