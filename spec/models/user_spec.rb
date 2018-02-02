4bccae580767820043bdc76a8d8b70dfd11091da
Alex Henegar <alex.henegar19@gmail.com>
2018-02-02 10:02:00 -0500

    add feature test for sign in and my queue

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,19 @@ describe User do
   it { should validate_presence_of(:full_name) }
   it { should validate_uniqueness_of(:email) }
   it { should have_many(:queue_items).order("position") }
+
+  describe '#queued_videos?' do
+    it 'returns true when the user queued the video' do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      Fabricate(:queue_item, user: user, video: video)
+      user.queued_video?(video).should be true
+    end
+
+    it 'returns false when the user hasn\'t queue the video' do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      user.queued_video?(video).should be false
+    end
+  end
 end
