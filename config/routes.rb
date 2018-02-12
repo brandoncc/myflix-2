6fc1d12d6ea574ee75c24137c4053f6be0e7e03c
Alex Henegar <alex.henegar19@gmail.com>
2018-02-12 11:43:44 -0500

    implement user show page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Myflix::Application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   get 'sign_out', to: 'sessions#destroy'
 
-  resources :users, only: [:create]
+  resources :users, only: [:show, :create]
   resources :sessions, only: [:create]
 end
