9563e80edac9edbf36f7600d6870802582c668d8
Alex Henegar <alex.henegar19@gmail.com>
2018-02-19 12:24:51 -0500

    route emails to admin gmail account in staging

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,20 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail from: "info@myflix.com", to: @user.email, subject: 'Welcome to MyFlix!'
+    mail from: "info@myflix.com", to: send_to(@user.email), subject: 'Welcome to MyFlix!'
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Please reset your password"
+    mail to: send_to(user.email), from: "info@myflix.com", subject: "Please reset your password"
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail to: @invitation.recipient_email, from: "info@myflix.com", subject: "Invitation to join MyFlix"
+    mail to: send_to(@invitation.recipient_email), from: "info@myflix.com", subject: "Invitation to join MyFlix"
+  end
+
+  def send_to(user)
+    Rails.env.staging? ? ENV['GMAIL_USERNAME'] : user
   end
 end
